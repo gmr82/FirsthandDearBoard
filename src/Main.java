8caea094af4b7e4430cdd33fc6c917593fa76ef4
gmr82 <gmr@ic.ufal.br>
2022-08-24 14:01:37 +0000

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ package src;
 
 public class Main
 {
-    public static void main (String[] args)
-    {
-        System.out.println("Opa!");
-    }
-    
+  public static void main (String[] args)
+  {
+    System.out.println("Opa!");
+  }
+   
 }
