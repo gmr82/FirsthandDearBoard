85bbfdff80d65586b8b9fdb5e836dac3a33f6465
gmr82 <gmr@ic.ufal.br>
2022-08-24 14:52:59 +0000

    just testing

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 package src;
 
+import src.entities.Employee;
+
 public class Main
 {
   public static void main (String[] args)
   {
-    System.out.println("Opa!");
+    Employee employee = new Employee(0, "Gustavo");
+
+    System.out.println(employee.toString());
+
   }
    
 }
